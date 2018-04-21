e05a7772d70f7bb6a6e27484fa53478fea12d2c3
Ressl Robert <r.ressl@safematix.com>
2018-04-21 13:54:58 +0200

    :pencil: change chef client version

--- provision/nodes.sh
+++ provision/nodes.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 localedef -v -c -i en_US -f UTF-8 en_US.UTF-8
 if [[ -f /etc/debian_version ]]; then
-  dpkg -i /tmp/chef_13.7.16-1_amd64.deb
+  dpkg -i /tmp/chef_14.0.202-1_amd64.deb
 else
-  rpm -Uvh  /tmp/chef-13.7.16-1.el7.x86_64.rpm
+  rpm -Uvh  /tmp/chef-14.0.202-1.el7.x86_64.rpm
 fi
 
 # configure hosts file for our internal network defined by Vagrantfile
