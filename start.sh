eb2641725eb66d0fe49499f81004d41c14574a23
Ressl Robert <r.ressl@safematix.com>
2018-08-01 16:27:23 +0200

    :pencil: update chef-client version

--- start.sh
+++ start.sh
@@ -7,8 +7,8 @@ declare -r LABHOME=$(pwd)
 chef_download() {
   # download packages
   wget -c -P ${LABHOME}/packages 'https://packages.chef.io/files/stable/chef-server/12.17.33/ubuntu/16.04/chef-server-core_12.17.33-1_amd64.deb'
-  wget -c -P ${LABHOME}/packages 'https://packages.chef.io/files/stable/chef/14.0.202/ubuntu/16.04/chef_14.0.202-1_amd64.deb'
-  wget -c -P ${LABHOME}/packages 'https://packages.chef.io/files/stable/chef/14.0.202/el/7/chef-14.0.202-1.el7.x86_64.rpm'
+  wget -c -P ${LABHOME}/packages 'https://packages.chef.io/files/stable/chef/14.3.37/ubuntu/18.04/chef_14.3.37-1_amd64.deb'
+  wget -c -P ${LABHOME}/packages 'https://packages.chef.io/files/stable/chef/14.3.37/el/7/chef-14.3.37-1.el7.x86_64.rpm'
 }
 
 chef_vagrant() {
